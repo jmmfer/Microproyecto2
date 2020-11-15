a4f1a86ad2e06d9e5ae8a3c98b12e3fe6dba000d
Fernando jose Mejia <>
2020-11-15 01:44:37 -0400

    commit final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {apiKey: "AIzaSyBoYao39H0Vk7FhQwyxETHCk0UtoBbldno",
+  authDomain: "microproyecto2-88ba2.firebaseapp.com",
+  databaseURL: "https://microproyecto2-88ba2.firebaseio.com",
+  projectId: "microproyecto2-88ba2",
+  storageBucket: "microproyecto2-88ba2.appspot.com",
+  messagingSenderId: "491560177577",
+  appId: "1:491560177577:web:5703ada9eb4889bddf3eb8",
+  measurementId: "G-MEW435DGDB"}
 };
